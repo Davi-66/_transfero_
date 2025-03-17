c42a8e17e9eebe10034c83115039aeae04a3e74e
Davi <drez3nde@gmail.com>
2025-03-17 16:38:37 -0300

    atualização do dia 17/03

--- sistema/views.py
+++ sistema/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# aqui irão ficar todas as views (controladores) referente ao sistema
+
+def index(request):
+    return render (
+        request, 
+        'sistema/index.html',
+    )
+
