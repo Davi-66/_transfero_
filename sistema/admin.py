2e01bb224b6ba311cff6fb505a96a9100f35faef
Davi <drez3nde@gmail.com>
2025-03-31 16:31:10 -0300

    commit do dia 31 de março

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,24 @@
+
 from django.contrib import admin
 
+from sistema import models
+
+# Aqui fica o registro do Usuário
+@admin.register(models.Usuário)
+class UsuárioAdmin(admin.ModelAdmin):
+    list_display = ('id','nome','sobrenome','email','ativo',)   
+
 # Register your models here.
+
+
+# aqui fica o registro do filme
+    
+@admin.register(models.Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_do_filme', 'ano_de_lancamento', 'estudio', 'genero',)
+
+# aqui fica o registro do genero
+
+@admin.register(models.Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,48 @@
+
+
+
+
+from django.utils import timezone
 from django.db import models
+
+# Aqui vai ficar a classe modelo do usuário
+# nome, sobrenome, cpf, telefone, email, foto, endereço.
+
+class Usuário(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    enredeco = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True)
+    imagem = models.ImageField(blank=True, upload_to= 'imagens/%Y/%m')
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
+    
+
+class Genero(models.Model):
+    nome = models.CharField(max_length = 50)
+    data_de_cadastro = models.DateTimeField(default= timezone.now)
+
+    def __str__(self):
+        return self.nome
+
+class Filme(models.Model):
+    nome_do_filme = models.CharField(max_length= 50)
+    ano_de_lancamento = models.DateField(default = timezone.now)
+    estudio = models.CharField(max_length = 50)
+    # genero = models.CharField(max_length = 50)
+    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, blank=True, )
+    sinopse = models.TextField()
+    data_de_cadastro = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.nome_do_filme
+
+
+
 
 # Create your models here.
